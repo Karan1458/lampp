24c17f845bb19af51c56952a3c47e74c4c6ccae5
Karan Sharma <karan1458@gmail.com>
2024-05-13 15:53:14 +0530

    Fix Permissions Script added.

--- php/fix-permissions.sh
+++ php/fix-permissions.sh
@@ -2,5 +2,9 @@
 
 # Set the correct permissions
 chown -R www-data:www-data /var/www/projects
-chmod -R 755 /var/www/projects
 
+# Fix Files 
+find /var/www/projects -type f -exec chmod 644 {} \; 
+
+# Fix Directories
+find /var/www/projects -type d -exec chmod 755 {} \; 
