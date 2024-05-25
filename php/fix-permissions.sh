c09ddd0ca037c14300c1ba03ee333a002a88eb90
Karan Sharma <karan1458@gmail.com>
2024-05-26 02:51:18 +0530

    Updated mailhog settings

--- php/fix-permissions.sh
+++ php/fix-permissions.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
 # Set the correct permissions
-chown -R www-data:www-data /var/www/projects
+chown -R www-data:www-data /var/www
 
 # Fix Files 
-find /var/www/projects -type f -exec chmod 644 {} \; 
+find /var/www/projects -type f -exec chmod 664 {} \; 
 
 # Fix Directories
-find /var/www/projects -type d -exec chmod 755 {} \; 
+find /var/www/projects -type d -exec chmod 775 {} \; 
